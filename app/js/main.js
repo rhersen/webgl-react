3fcaafac408dd85a6f6aba839198fb4692e7f8c0
Rudolf Hersen <rhersen@gmail.com>
2014-03-11 22:30:26 +0100

    added buttons

--- app/js/main.js
+++ app/js/main.js
@@ -50,29 +50,67 @@ var ReactRoot = React.createClass({
    getInitialState: function () {
       return {
          millis: Date.now(),
-         timezoneOffset: new Date().getTimezoneOffset()
+         timezoneOffset: new Date().getTimezoneOffset(),
+         request: 0
       };
+   },
+
+   componentDidMount: function () {
+      this.setState({
+         request: requestAnimationFrame(this.tick)
+      });
+   },
+
+   componentWillUnmount: function () {
+      this.stop();
    },
 
    tick: function () {
       this.setState({
-         millis: Date.now()
+         millis: Date.now(),
+         request: requestAnimationFrame(this.tick)
       });
-      this.request = requestAnimationFrame(this.tick);
    },
 
-   componentDidMount: function () {
-      this.request = requestAnimationFrame(this.tick);
+   stop: function () {
+      cancelAnimationFrame(this.state.request);
+      this.setState({ request: 0 });
    },
 
-   componentWillUnmount: function () {
-      cancelAnimationFrame(this.request);
+   gmt: function () {
+      this.setState({timezoneOffset: 0});
+   },
+
+   local: function () {
+      this.setState({timezoneOffset: new Date().getTimezoneOffset()});
    },
 
    render: function () {
-      return Clock({
-         millis: this.state.millis,
-         timezoneOffset: this.state.timezoneOffset
-      });
+      return React.DOM.div({},
+         React.DOM.button({
+               onClick: this.tick,
+               disabled: !!this.state.request
+            },
+            'start'),
+         React.DOM.button({
+               onClick: this.stop,
+               disabled: !this.state.request
+            },
+            'stop'),
+         React.DOM.button({
+               onClick: this.gmt,
+               disabled: !this.state.timezoneOffset
+            },
+            'gmt'),
+         React.DOM.button({
+               onClick: this.local,
+               disabled: !!this.state.timezoneOffset
+            },
+            'local'),
+         Clock({
+            millis: this.state.millis,
+            timezoneOffset: this.state.timezoneOffset
+         })
+      );
    }
 });
