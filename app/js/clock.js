2abe0774643005e4e7b8840e391be91c91c84f7f
Rudolf Hersen <rhersen@gmail.com>
2014-03-12 19:57:24 +0100

    SVG transform rotate instead of explicit trigonometry JSHint

--- app/js/clock.js
+++ app/js/clock.js
@@ -1,6 +1,6 @@
 var Clock = React.createClass({
    getAngle: function (cycleTime) {
-      return 2 * Math.PI * (this.getMillis() % cycleTime) / cycleTime;
+      return 360 * (this.getMillis() % cycleTime) / cycleTime;
    },
 
    getMillis: function () {
@@ -9,13 +9,11 @@ var Clock = React.createClass({
 
    render: function () {
       return React.DOM.svg({
-            viewBox: "-1.5 -1.5 3 3"
+            viewBox: '-1.5 -1.5 3 3'
          },
          React.DOM.circle({
-            cx: 0,
-            cy: 0,
             r: 1,
-            fill: "ivory"}),
+            fill: 'ivory'}),
          Hand({
             length: 0.7,
             width: 0.1,
